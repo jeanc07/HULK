9a9adc3343a996708fef5733af99b6fcf0a4c519
jeanc07 <jeancgw@icloud.com>
2023-12-15 23:22:55 -0500

    StringToken class of HULK

---
+++ Parsing/StringToken.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// CLase utilizada para almacenar cádenas de texto usadas por el usuario.
+/// </summary>
+public class StringToken : TokenValue
+{
+    public StringToken(string? nombre, string valor) : base(nombre, valor)
+    {
+
+    }
+
+}
