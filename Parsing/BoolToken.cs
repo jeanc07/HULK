72ca8c1b602435868cb2d6caebb53031c49cd1a5
jeanc07 <jeancgw@icloud.com>
2023-12-15 23:21:01 -0500

    BoolToken class

---
+++ Parsing/BoolToken.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// Clase usada para guardar las variables de tipo bool.
+/// </summary>
+public class BoolToken : TokenValue
+{
+    public BoolToken(string? nombre, string valor) : base(nombre, valor)
+    {
+
+    }
+}
