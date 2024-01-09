ed836d2f1577538ecf6cf82ee1643cd3cb688a8e
jeanc07 <jeancgw@icloud.com>
2024-01-08 22:01:14 -0500

    RecursiveFunction class

---
+++ Parsing/RecursiveFunction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingLibrary
+{
+    public class RecursiveFunction : Function
+    {
+        public RecursiveFunction(string nombre, List<Variable> parametro, List<string> cuerpo) : base(nombre, parametro, cuerpo)
+        {
+
+        }
+
+        public RecursiveFunction() : base()
+        {
+
+        }
+    }
+}
