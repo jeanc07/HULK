2744473f6ad38d36557b6a3b53b5acc0a624e509
jeanc07 <jeancgw@icloud.com>
2024-01-08 22:01:28 -0500

    Changes for Semantic class

--- Parsing/Semantic.cs
+++ Parsing/Semantic.cs
@@ -14,15 +14,17 @@ public class Semantic
         {
             token = currentTokens.ElementAt(i);
             List<SpecialTokensClass> specials = lex.SpecialTokensClass();
-
+            List<Variable> variables = lex.Variables();
+            List<StringToken> stringTokens = lex.StringTokens();
+            List<Function> functions = lex.Functions();
             if (lex.Operators().Any(x => x == token && token != "="))
             {
-                if (!double.TryParse(currentTokens.ElementAt(i-1),out _) && !specials.Any(x =>x is Variable && ((Variable)x).Nombre == currentTokens.ElementAt(i-1) && (((Variable)x).Tipo == "int" || ((Variable)x).Tipo == null))
-                && currentTokens.ElementAt(i-1) != ")" && currentTokens.ElementAt(i-1) != "(")
+                if (!double.TryParse(currentTokens.ElementAt(i-1),out _) && !variables.Any(x =>x is Variable && ((Variable)x).Nombre == currentTokens.ElementAt(i-1) && (((Variable)x).Tipo == "int" || ((Variable)x).Tipo == null)) && !lex.MathTokens().Contains(currentTokens.ElementAt(i-1))
+                && currentTokens.ElementAt(i-1) != ")" && currentTokens.ElementAt(i-1) != "(" && !functions.Any(x => x is Function && x.Nombre == currentTokens.ElementAt(i-1)) && !functions.Any(x => x is Function && ((Function)x).Parametro.Any(x => x.Nombre == currentTokens.ElementAt(i-1))))
                 {
                     bad = true;
-                }else if (!double.TryParse(currentTokens.ElementAt(i+1),out _) && !specials.Any(x =>x is Variable && ((Variable)x).Nombre == currentTokens.ElementAt(i+1) && (((Variable)x).Tipo == "int" || ((Variable)x).Tipo == null))
-                && currentTokens.ElementAt(i+1) != ")" && currentTokens.ElementAt(i+1) != "(" && !specials.Any(x => x is Function && x.Nombre == currentTokens.ElementAt(i+1)))
+                }else if (!double.TryParse(currentTokens.ElementAt(i+1),out _) && !variables.Any(x =>x is Variable && ((Variable)x).Nombre == currentTokens.ElementAt(i+1) && (((Variable)x).Tipo == "int" || ((Variable)x).Tipo == null)) && !lex.MathTokens().Contains(currentTokens.ElementAt(i+1))
+                && currentTokens.ElementAt(i+1) != ")" && currentTokens.ElementAt(i+1) != "(" && !functions.Any(x => x is Function && x.Nombre == currentTokens.ElementAt(i+1)) && !functions.Any(x => x is Function && ((Function)x).Parametro.Any(x => x.Nombre == currentTokens.ElementAt(i+1))))
                 {
                     bad = true;
                 }
@@ -53,56 +55,58 @@ public class Semantic
                 {
                     if ((!double.TryParse(templist.ElementAt(j),out _) && !lex.MathTokens().Contains(templist[j])
                     && !lex.Operators().Contains(templist[j]) && !lex.Symbols().Contains(templist[j]))
-                    && !specials.Any(x => ((Variable)x).Nombre == templist[j] && ((Variable)x).Tipo == "int"))
+                    && !variables.Any(x => x is Variable && ((Variable)x).Nombre == templist[j] && ((Variable)x).Tipo == "int")
+                    && !functions.Any(x => x is Function && ((Function)x).Parametro.Any(x => x.Nombre == templist[j])))
                     {
                         bad = true;
                     }
                 }
-            }else if (token == "==")
+            }else if (lex.ConditionOperators().Any(x => x == token))
             {
                 bool isbool = false;
                 bool isint = false;
 
-                if (double.TryParse(currentTokens.ElementAt(i-1),out _) || specials.Any(x => x is Variable && ((Variable)x).Nombre == currentTokens.ElementAt(i-1) && ((Variable)x).Tipo == "int"))
+                if (double.TryParse(currentTokens.ElementAt(i-1),out _) || variables.Any(x => x is Variable && ((Variable)x).Nombre == currentTokens.ElementAt(i-1) && ((Variable)x).Tipo == "int"))
                 {
                     isint = true;
-                }else if (specials.Any(x => x is StringToken && ((StringToken)x).Valor == token))
+                }else if (stringTokens.Any(x => x is StringToken && ((StringToken)x).Valor == token))
                 {
                     isint = false;
                     isbool = false;
-                }else if (lex.BoolValues().Contains(currentTokens.ElementAt(i-1)) || specials.Any(x =>x is Variable &&((Variable)x).Nombre == currentTokens.ElementAt(i-1) && (((Variable)x).Tipo == "bool")))
+                }else if (lex.BoolValues().Contains(currentTokens.ElementAt(i-1)) || variables.Any(x =>x is Variable &&((Variable)x).Nombre == currentTokens.ElementAt(i-1) && (((Variable)x).Tipo == "bool")))
                 {
                     isbool = true;
                 }
 
                 if (isint)
                 {
-                    if (!double.TryParse(currentTokens.ElementAt(i+1),out _) && !specials.Any(x => ((Variable)x).Nombre == currentTokens.ElementAt(i+1) && ((Variable)x).Tipo == "int"))
+                    if (!double.TryParse(currentTokens.ElementAt(i+1),out _) && !variables.Any(x => ((Variable)x).Nombre == currentTokens.ElementAt(i+1) && ((Variable)x).Tipo == "int"))
                     {
                         bad = true;
                     }
                 }else if (isbool)
                 {
-                    if (!lex.BoolValues().Contains(currentTokens.ElementAt(i+1)) && !specials.Any(x => ((Variable)x).Nombre == currentTokens.ElementAt(i+1) && (((Variable)x).Tipo == "bool")))
+                    if (!lex.BoolValues().Contains(currentTokens.ElementAt(i+1)) && !variables.Any(x => ((Variable)x).Nombre == currentTokens.ElementAt(i+1) && (((Variable)x).Tipo == "bool")))
                     {
                         bad = true;
                     }
                 }else
                 {
-                    if ((double.TryParse(currentTokens.ElementAt(i+1),out _) || lex.BoolValues().Contains(currentTokens.ElementAt(i+1)) 
-                    || specials.Any(x =>x is Variable && ((Variable)x).Nombre == currentTokens.ElementAt(i+1) && (((Variable)x).Tipo == "int" || (((Variable)x).Tipo == "bool")))))
-                    {
-                        bad = true;
-                    }
+                    if(functions.Any(x => x is Function && ((Function)x).Parametro.Any(x => x.Nombre == currentTokens.ElementAt(i+1))))
+                        if ((double.TryParse(currentTokens.ElementAt(i+1),out _) || lex.BoolValues().Contains(currentTokens.ElementAt(i+1)) 
+                        || variables.Any(x =>x is Variable && ((Variable)x).Nombre == currentTokens.ElementAt(i+1) && (((Variable)x).Tipo == "int" || (((Variable)x).Tipo == "bool")))))
+                        {
+                            bad = true;
+                        }
                 }         
             }else if (token == "@")
             {
-                if (!double.TryParse(currentTokens.ElementAt(i-1),out _) && !specials.Any(x =>x is Variable &&((Variable)x).Nombre == currentTokens.ElementAt(i-1) && (((Variable)x).Tipo == "int") || ((Variable)x).Tipo == "string")
-                && !specials.Any(x => x is StringToken && ((StringToken)x).Valor == currentTokens.ElementAt(i-1)))
+                if (!double.TryParse(currentTokens.ElementAt(i-1),out _) && !variables.Any(x =>x is Variable &&((Variable)x).Nombre == currentTokens.ElementAt(i-1) && (((Variable)x).Tipo == "int") || ((Variable)x).Tipo == "string")
+                && !stringTokens.Any(x => x is StringToken && ((StringToken)x).Valor == currentTokens.ElementAt(i-1)))
                 {
                     bad = true;
-                }else if (!double.TryParse(currentTokens.ElementAt(i+1),out _) && !specials.Any(x =>x is Variable &&((Variable)x).Nombre == currentTokens.ElementAt(i+1) && (((Variable)x).Tipo == "int") ||((Variable)x).Tipo == "string")
-                && !specials.Any(x => x is StringToken && ((StringToken)x).Valor == currentTokens.ElementAt(i+1)))
+                }else if (!double.TryParse(currentTokens.ElementAt(i+1),out _) && !variables.Any(x =>x is Variable &&((Variable)x).Nombre == currentTokens.ElementAt(i+1) && (((Variable)x).Tipo == "int") ||((Variable)x).Tipo == "string")
+                && !stringTokens.Any(x => x is StringToken && ((StringToken)x).Valor == currentTokens.ElementAt(i+1)))
                 {
                     bad = true;
                 }
